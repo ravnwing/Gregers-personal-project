cf64b806c63e77dab935b0a261ab10510b6072e8
ravnwing <ravnwing@gmail.com>
2016-04-20 10:47:58 -0400

    djinn collection, randomize, and render functioning

--- five-tribes/client/main.js
+++ five-tribes/client/main.js
@@ -3,28 +3,64 @@ Template.gameStart.helpers({
     tempDeck = [];
     count = 0;
     tempDeck = TileList.find().fetch();
-    tileRow = [];
+    tileRow0 = [];
+    tileRow1 = [];
+    tileRow2 = [];
+    tileRow3 = [];
+    tileRow4 = [];
     tileDeck = [];
 
     while (tempDeck.length != 0){
-      var rand = Math.floor(Math.random()*tempDeck.length);
-      if (rand != 1) {
-        tileDeck.push(tempDeck[rand]);
-        tempDeck.splice(rand, 1);
-        count++;
+      if (count < 6){
+        var rand = Math.floor(Math.random()*tempDeck.length);
+        if (rand != 1) {
+          tileRow0.push(tempDeck[rand]);
+          tempDeck.splice(rand, 1);
+          count++;
+        }
       }
-      // if (count == 6){
-      //   tileDeck.push(tileRow);
-      //   count = 0;
-      //   tileRow = [];
-      // }
+      else if(count < 12){
+        var rand = Math.floor(Math.random()*tempDeck.length);
+        if (rand != 1) {
+          tileRow1.push(tempDeck[rand]);
+          tempDeck.splice(rand, 1);
+          count++;
+        }
+      }
+      else if (count < 18) {
+        var rand = Math.floor(Math.random()*tempDeck.length);
+        if (rand != 1) {
+          tileRow2.push(tempDeck[rand]);
+          tempDeck.splice(rand, 1);
+          count++;
+        }
+      }
+      else if (count < 24){
+        var rand = Math.floor(Math.random()*tempDeck.length);
+        if (rand != 1) {
+          tileRow3.push(tempDeck[rand]);
+          tempDeck.splice(rand, 1);
+          count++;
+        }
+      }
+      else {
+        var rand = Math.floor(Math.random()*tempDeck.length);
+        if (rand != 1) {
+          tileRow4.push(tempDeck[rand]);
+          tempDeck.splice(rand, 1);
+          count++;
+        }
+      }
     };
-    Session.set("tilePass", tileDeck);
+    Session.set("row0Pass", tileRow0);
+    Session.set("row1Pass", tileRow1);
+    Session.set("row2Pass", tileRow2);
+    Session.set("row3Pass", tileRow3);
+    Session.set("row4Pass", tileRow4);
+
 
     tempDeck = MerchList.find().fetch();
     merchDeck = [];
-    merchant = [];
-
 
     while (tempDeck.length != 0){
       var rand = Math.floor(Math.random()*tempDeck.length);
@@ -36,7 +72,19 @@ Template.gameStart.helpers({
     Session.set("merchPass", merchDeck);
 
 
-  },
+  tempDeck = DjinnList.find().fetch();
+  djinnDeck = [];
+
+  while (tempDeck.length != 0){
+    var rand = Math.floor(Math.random()*tempDeck.length);
+    if (rand != 1) {
+      djinnDeck.push(tempDeck[rand]);
+      tempDeck.splice(rand, 1);
+    }
+  };
+  Session.set("djinnPass", djinnDeck);
+},
+
 
 
   'tilePosition': function(){
@@ -53,24 +101,27 @@ Template.gameStart.helpers({
 });
 
 Template.tile.helpers({
-  'rowPlace': function(){
-    var tileDeck = Session.get("tilePass");
-    var tileRow = [];
-
-    for (var i = 0; i < 6; i++) {
-      tileRow.push(tileDeck.pop());
-    }
-
-    return tileRow;
+  'row0Place': function(){
+    var tileRow0 = Session.get("row0Pass");
+    return tileRow0;
+  },
+  'row1Place': function(){
+    var tileRow1 = Session.get("row1Pass");
+    return tileRow1;
+  },
+  'row2Place': function(){
+    var tileRow2 = Session.get("row2Pass");
+    return tileRow2;
+  },
+  'row3Place': function(){
+    var tileRow3 = Session.get("row3Pass");
+    return tileRow3;
+  },
+  'row4Place': function(){
+    var tileRow4 = Session.get("row4Pass");
+    return tileRow4;
   },
 
-
-
-
-  'tilePlace': function(){
-    var tileDeck = Session.get("tilePass");
-    return tileDeck;
-  },
   'rowCounter': function(){
     count++;
     space = false
@@ -95,3 +146,14 @@ Template.merch.helpers({
 
   },
 });
+
+Template.djinn.helpers({
+  'djinnShow': function(){
+    djinnDeck = Session.get("djinnPass")
+    var availDjinn = [];
+    while (availDjinn.length <3){
+      availDjinn.push(djinnDeck.pop());
+    }
+    return availDjinn;
+  }
+})
